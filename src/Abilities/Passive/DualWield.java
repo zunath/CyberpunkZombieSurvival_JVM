8607f04b0e3072327c85c776d9402f9f61f6766a
zunath <coolty3001@yahoo.com>
2017-01-02 14:24:38 -0600

    Dual Wield: Unequip and re-equip any off hand weapon after unequipping or equipping the ability.

--- src/Abilities/Passive/DualWield.java
+++ src/Abilities/Passive/DualWield.java
@@ -3,7 +3,10 @@ package Abilities.Passive;
 import Abilities.IAbility;
 import NWNX.NWNX_Funcs;
 import org.nwnx.nwnx2.jvm.NWObject;
+import org.nwnx.nwnx2.jvm.NWScript;
+import org.nwnx.nwnx2.jvm.Scheduler;
 import org.nwnx.nwnx2.jvm.constants.Feat;
+import org.nwnx.nwnx2.jvm.constants.InventorySlot;
 
 public class DualWield implements IAbility {
     @Override
@@ -40,11 +43,39 @@ public class DualWield implements IAbility {
     public void OnEquip(NWObject oPC) {
         NWNX_Funcs.AddKnownFeat(oPC, Feat.AMBIDEXTERITY, 0);
         NWNX_Funcs.AddKnownFeat(oPC, Feat.TWO_WEAPON_FIGHTING, 0);
+
+        final NWObject offHand = NWScript.getItemInSlot(InventorySlot.LEFTHAND, oPC);
+
+        if(offHand != NWObject.INVALID.INVALID)
+        {
+            Scheduler.assign(oPC, new Runnable() {
+                @Override
+                public void run() {
+                    NWScript.actionUnequipItem(offHand);
+                    NWScript.actionEquipItem(offHand, InventorySlot.LEFTHAND);
+                }
+            });
+        }
+
     }
 
     @Override
     public void OnUnequip(NWObject oPC) {
         NWNX_Funcs.RemoveKnownFeat(oPC, Feat.AMBIDEXTERITY);
         NWNX_Funcs.RemoveKnownFeat(oPC, Feat.TWO_WEAPON_FIGHTING);
+
+        final NWObject offHand = NWScript.getItemInSlot(InventorySlot.LEFTHAND, oPC);
+
+        if(offHand != NWObject.INVALID)
+        {
+            Scheduler.assign(oPC, new Runnable() {
+                @Override
+                public void run() {
+                    NWScript.actionUnequipItem(offHand);
+                    NWScript.actionEquipItem(offHand, InventorySlot.LEFTHAND);
+                }
+            });
+        }
+
     }
 }
