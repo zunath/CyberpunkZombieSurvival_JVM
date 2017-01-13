d6abc21e9d9eec08749baa4df8949ce1067c6026
zunath <coolty3001@yahoo.com>
2017-01-13 08:06:42 -0500

    Add workaround for NWN glitch which allows extra attacks

--- src/Event/Module/OnPlayerEquipItem.java
+++ src/Event/Module/OnPlayerEquipItem.java
@@ -1,7 +1,14 @@
 package Event.Module;
 import Common.IScriptEventHandler;
 import GameSystems.*;
+import Helper.ColorToken;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.nwnx.nwnx2.jvm.*;
+
+import java.util.Objects;
 
 @SuppressWarnings("unused")
 public class OnPlayerEquipItem implements IScriptEventHandler {
@@ -16,5 +23,41 @@ public class OnPlayerEquipItem implements IScriptEventHandler {
         DurabilitySystem.OnModuleEquip();
 		ArmorSystem.OnModuleEquipItem();
 		InventorySystem.OnModuleEquipItem();
+
+		HandleEquipmentSwappingDelay();
 	}
+
+	// Players abuse a bug in NWN which allows them to gain an extra attack.
+	// To work around this I force a delay before they can equip weapons during combat.
+	private void HandleEquipmentSwappingDelay()
+	{
+		NWObject oPC = NWScript.getPCItemLastEquippedBy();
+		final NWObject oItem = NWScript.getPCItemLastEquipped();
+		if(!NWScript.getIsInCombat(oPC)) return;
+
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ssz");
+		String delayUntilTimestamp = NWScript.getLocalString(oPC, "TEMP_DELAY_WEAPON_SWITCHING");
+		long delayMillis = 0;
+		if(!Objects.equals(delayUntilTimestamp, ""))
+		{
+			DateTime dateTime = formatter.withZone(DateTimeZone.UTC).parseDateTime(delayUntilTimestamp);
+			delayMillis = dateTime.getMillis();
+		}
+
+		if(DateTime.now(DateTimeZone.UTC).getMillis() < delayMillis)
+		{
+			NWScript.sendMessageToPC(oPC, ColorToken.Red() + "You must wait longer before equipping that item during combat." + ColorToken.End());
+
+			Scheduler.assign(oPC, new Runnable() {
+				@Override
+				public void run() {
+					NWScript.actionUnequipItem(oItem);
+				}
+			});
+		}
+
+		delayUntilTimestamp = DateTime.now(DateTimeZone.UTC).plusSeconds(1).toString(formatter);
+		NWScript.setLocalString(oPC, "TEMP_DELAY_WEAPON_SWITCHING", delayUntilTimestamp);
+	}
+
 }
