bef60c1697245a984d0b23541ed1bdb23a4a1f1b
zunath <coolty3001@yahoo.com>
2017-01-26 16:07:32 -0500

    Apply AC decrease effect for poison effect.

--- src/CustomEffect/PoisonEffect.java
+++ src/CustomEffect/PoisonEffect.java
@@ -1,7 +1,9 @@
 package CustomEffect;
 
+import org.nwnx.nwnx2.jvm.NWEffect;
 import org.nwnx.nwnx2.jvm.NWObject;
 import org.nwnx.nwnx2.jvm.NWScript;
+import org.nwnx.nwnx2.jvm.constants.Ac;
 import org.nwnx.nwnx2.jvm.constants.DamagePower;
 import org.nwnx.nwnx2.jvm.constants.DamageType;
 import org.nwnx.nwnx2.jvm.constants.DurationType;
@@ -13,5 +15,8 @@ public class PoisonEffect implements ICustomEffectHandler {
     public void run(NWObject oCaster, NWObject oTarget) {
         int damage = ThreadLocalRandom.current().nextInt(3, 7);
         NWScript.applyEffectToObject(DurationType.INSTANT, NWScript.effectDamage(damage, DamageType.MAGICAL, DamagePower.NORMAL), oTarget, 0.0f);
+
+        NWEffect decreaseAC = NWScript.effectACDecrease(2, Ac.DODGE_BONUS, Ac.VS_DAMAGE_TYPE_ALL);
+        NWScript.applyEffectToObject(DurationType.TEMPORARY, decreaseAC, oTarget, 6.1f);
     }
 }
