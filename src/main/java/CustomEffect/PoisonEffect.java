a3bc7d7dc432532aa2feda2f347c49654d33c7b5
zunath <coolty3001@yahoo.com>
2018-03-08 13:00:22 +0800

    Revert the custom effect linking change to fix scheduler issues. Will revisit at a later point.

--- src/main/java/CustomEffect/PoisonEffect.java
+++ src/main/java/CustomEffect/PoisonEffect.java
@@ -3,7 +3,6 @@ package CustomEffect;
 import org.nwnx.nwnx2.jvm.NWEffect;
 import org.nwnx.nwnx2.jvm.NWObject;
 import org.nwnx.nwnx2.jvm.NWScript;
-import org.nwnx.nwnx2.jvm.Scheduler;
 import org.nwnx.nwnx2.jvm.constants.*;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -11,13 +10,12 @@ import java.util.concurrent.ThreadLocalRandom;
 public class PoisonEffect implements ICustomEffectHandler {
     @Override
     public void run(NWObject oCaster, NWObject oTarget) {
-        Scheduler.assign(oCaster, () -> {
-            int damage = ThreadLocalRandom.current().nextInt(3, 7);
-            NWScript.applyEffectToObject(DurationType.INSTANT, NWScript.effectDamage(damage, DamageType.MAGICAL, DamagePower.NORMAL), oTarget, 0.0f);
-            NWEffect decreaseAC = NWScript.effectACDecrease(2, Ac.DODGE_BONUS, Ac.VS_DAMAGE_TYPE_ALL);
-            NWScript.applyEffectToObject(DurationType.TEMPORARY, decreaseAC, oTarget, 6.1f);
+        int damage = ThreadLocalRandom.current().nextInt(3, 7);
+        NWScript.applyEffectToObject(DurationType.INSTANT, NWScript.effectDamage(damage, DamageType.MAGICAL, DamagePower.NORMAL), oTarget, 0.0f);
 
-            NWScript.applyEffectToObject(Duration.TYPE_INSTANT, NWScript.effectVisualEffect(Vfx.IMP_ACID_S, false), oTarget, 0.0f);
-        });
+        NWEffect decreaseAC = NWScript.effectACDecrease(2, Ac.DODGE_BONUS, Ac.VS_DAMAGE_TYPE_ALL);
+        NWScript.applyEffectToObject(DurationType.TEMPORARY, decreaseAC, oTarget, 6.1f);
+
+        NWScript.applyEffectToObject(Duration.TYPE_INSTANT, NWScript.effectVisualEffect(Vfx.IMP_ACID_S, false), oTarget, 0.0f);
     }
 }
