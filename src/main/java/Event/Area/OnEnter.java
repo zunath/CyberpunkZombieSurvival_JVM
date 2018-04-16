f493f6aea8243145345997fbb7493f4e1f139cae
zunath <coolty3001@yahoo.com>
2018-04-16 14:44:28 +0800

    Ignore sanctuary effects if player enters OOC area. This should prevent it from being removed automatically after a server reset.

--- src/main/java/Event/Area/OnEnter.java
+++ src/main/java/Event/Area/OnEnter.java
@@ -13,45 +13,47 @@ import org.nwnx.nwnx2.jvm.constants.*;
 
 import java.util.Objects;
 
+import static org.nwnx.nwnx2.jvm.NWScript.*;
+
 @SuppressWarnings("UnusedDeclaration")
 public class OnEnter implements IScriptEventHandler {
     @Override
     public void runScript(NWObject oArea) {
         SpawnSystem spawnSystem = new SpawnSystem();
 
-        NWObject oPC = NWScript.getEnteringObject();
+        NWObject oPC = getEnteringObject();
         MigrationSystem.OnAreaEnter(oPC);
 
         LoadLocation(oPC, oArea);
         SaveLocation(oPC, oArea);
-        ApplySanctuaryEffects(oPC);
+        ApplySanctuaryEffects(oPC, oArea);
         spawnSystem.OnAreaEnter(oArea);
         AdjustCamera(oPC);
         ProfessionSystem.OnAreaEnter(oArea);
 
-        if(NWScript.getIsObjectValid(oPC) && NWScript.getIsPC(oPC) && !NWScript.getIsDM(oPC))
-            NWScript.exportSingleCharacter(oPC);
+        if(getIsObjectValid(oPC) && getIsPC(oPC) && !getIsDM(oPC))
+            exportSingleCharacter(oPC);
     }
 
     private void CheckForMovement(final NWObject oPC, final NWLocation location)
     {
-        if(!NWScript.getIsObjectValid(oPC) || NWScript.getIsDead(oPC)) return;
+        if(!getIsObjectValid(oPC) || getIsDead(oPC)) return;
 
-        NWLocation currentLocation = NWScript.getLocation(oPC);
-        String areaResref = NWScript.getResRef(NWScript.getArea(oPC));
+        NWLocation currentLocation = getLocation(oPC);
+        String areaResref = getResRef(getArea(oPC));
 
-        if(!Objects.equals(areaResref, NWScript.getResRef(location.getArea())) ||
+        if(!Objects.equals(areaResref, getResRef(location.getArea())) ||
            currentLocation.getFacing() != location.getFacing() ||
            currentLocation.getX() != location.getX() ||
            currentLocation.getY() != location.getY() ||
            currentLocation.getZ() != location.getZ())
         {
-            for(NWEffect effect : NWScript.getEffects(oPC))
+            for(NWEffect effect : getEffects(oPC))
             {
-                int type = NWScript.getEffectType(effect);
+                int type = getEffectType(effect);
                 if(type == EffectType.DAMAGE_REDUCTION || type == EffectType.SANCTUARY)
                 {
-                    NWScript.removeEffect(oPC, effect);
+                    removeEffect(oPC, effect);
                 }
             }
 
@@ -61,34 +63,35 @@ public class OnEnter implements IScriptEventHandler {
         Scheduler.delay(oPC, 1000, () -> CheckForMovement(oPC, location));
     }
 
-    private void ApplySanctuaryEffects(final NWObject oPC)
+    private void ApplySanctuaryEffects(final NWObject oPC, final NWObject oArea)
     {
-        if(!NWScript.getIsPC(oPC) || NWScript.getIsDM(oPC) || NWScript.getIsDMPossessed(oPC)) return;
-        if(NWScript.getCurrentHitPoints(oPC) <= 0) return;
+        if(!getIsPC(oPC) || getIsDM(oPC) || getIsDMPossessed(oPC)) return;
+        if(getCurrentHitPoints(oPC) <= 0) return;
+        if(getResRef(oArea).equals("ooc_area")) return;
 
-        NWScript.applyEffectToObject(DurationType.PERMANENT, NWScript.effectSanctuary(99), oPC, 0.0f);
-        NWScript.applyEffectToObject(DurationType.PERMANENT, NWScript.effectDamageReduction(50, DamagePowerPlus.TWENTY, 0), oPC, 0.0f);
-        final NWLocation location = NWScript.getLocation(oPC);
+        applyEffectToObject(DurationType.PERMANENT, effectSanctuary(99), oPC, 0.0f);
+        applyEffectToObject(DurationType.PERMANENT, effectDamageReduction(50, DamagePowerPlus.TWENTY, 0), oPC, 0.0f);
+        final NWLocation location = getLocation(oPC);
 
         Scheduler.delay(oPC, 3500, () -> CheckForMovement(oPC, location));
     }
 
     private void SaveLocation(NWObject oPC, NWObject oArea)
     {
-        if(!NWScript.getIsPC(oPC) || NWScript.getIsDM(oPC) || NWScript.getIsDMPossessed(oPC)) return;
+        if(!getIsPC(oPC) || getIsDM(oPC) || getIsDMPossessed(oPC)) return;
 
-        String sTag = NWScript.getTag(oArea);
+        String sTag = getTag(oArea);
         if(!sTag.equals("ooc_area"))
         {
             PlayerGO pcGO = new PlayerGO(oPC);
-            NWLocation location = NWScript.getLocation(oPC);
+            NWLocation location = getLocation(oPC);
             PlayerRepository repo = new PlayerRepository();
             PlayerEntity entity = repo.GetByPlayerID(pcGO.getUUID());
             entity.setLocationAreaTag(sTag);
             entity.setLocationX(location.getX());
             entity.setLocationY(location.getY());
             entity.setLocationZ(location.getZ());
-            entity.setLocationOrientation(NWScript.getFacing(oPC));
+            entity.setLocationOrientation(getFacing(oPC));
 
             repo.save(entity);
         }
@@ -96,41 +99,41 @@ public class OnEnter implements IScriptEventHandler {
 
     private void LoadLocation(NWObject oPC, NWObject oArea)
     {
-        if(!NWScript.getIsPC(oPC) || NWScript.getIsDM(oPC) || NWScript.getIsDMPossessed(oPC)) return;
+        if(!getIsPC(oPC) || getIsDM(oPC) || getIsDMPossessed(oPC)) return;
 
-        if(NWScript.getTag(oArea).equals("ooc_area"))
+        if(getTag(oArea).equals("ooc_area"))
         {
             PlayerGO pcGO = new PlayerGO(oPC);
             PlayerEntity entity = new PlayerRepository().GetByPlayerID(pcGO.getUUID());
-            NWObject area = NWScript.getObjectByTag(entity.getLocationAreaTag(), 0);
+            NWObject area = getObjectByTag(entity.getLocationAreaTag(), 0);
             final NWLocation location = new NWLocation(area,
                     entity.getLocationX(),
                     entity.getLocationY(),
                     entity.getLocationZ(),
                     entity.getLocationOrientation());
 
-            Scheduler.assign(oPC, () -> NWScript.actionJumpToLocation(location));
+            Scheduler.assign(oPC, () -> actionJumpToLocation(location));
         }
     }
 
     private void ShowMap(NWObject oPC, NWObject oArea)
     {
-        int keyItemID = NWScript.getLocalInt(oArea, "MAP_ID");
-        boolean areaShowsMap = NWScript.getLocalInt(oArea, "SHOW_MAP") == 1;
+        int keyItemID = getLocalInt(oArea, "MAP_ID");
+        boolean areaShowsMap = getLocalInt(oArea, "SHOW_MAP") == 1;
         boolean hasKeyItem = KeyItemSystem.PlayerHasKeyItem(oPC, keyItemID);
 
         if(areaShowsMap || hasKeyItem)
         {
-            NWScript.exploreAreaForPlayer(oArea, oPC, true);
+            exploreAreaForPlayer(oArea, oPC, true);
         }
     }
 
     private void AdjustCamera(NWObject oPC)
     {
-        if(!NWScript.getIsPC(oPC)) return;
+        if(!getIsPC(oPC)) return;
 
-        float facing = NWScript.getFacing(oPC) - 180;
-        NWScript.setCameraFacing(facing, 1.0f, 1.0f, CameraTransitionType.SNAP);
+        float facing = getFacing(oPC) - 180;
+        setCameraFacing(facing, 1.0f, 1.0f, CameraTransitionType.SNAP);
     }
 
 }
